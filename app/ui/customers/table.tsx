32e27bce2e2072562462fb1cc758c90f933e4eb2
apm30 <m.ajaypradeep@icloud.com>
2023-11-26 14:58:01 +0000

    Refactor CustomersTable import

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import {
+  CustomersTable as CT,
+  FormattedCustomersTable,
+} from '@/app/lib/definitions';
 
-export default async function CustomersTablee({
+export default async function CustomersTable({
   customers,
 }: {
   customers: FormattedCustomersTable[];
