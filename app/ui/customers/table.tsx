498abfad79194506e85efdc0316e4e768e176ad0
apm30 <m.ajaypradeep@icloud.com>
2023-11-26 14:57:27 +0000

    Fix typo in CustomersTable function name

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -3,7 +3,7 @@ import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
 
-export default async function CustomersTable({
+export default async function CustomersTablee({
   customers,
 }: {
   customers: FormattedCustomersTable[];
