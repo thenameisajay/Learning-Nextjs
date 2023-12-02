0f6c04175d60355cdabdd784ff4af15b53232e10
apm30 <m.ajaypradeep@icloud.com>
2023-12-02 15:22:41 +0000

    Add loading component and dashboard page

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+// A skeleton is a placeholder for content that hasn't loaded yet.
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
